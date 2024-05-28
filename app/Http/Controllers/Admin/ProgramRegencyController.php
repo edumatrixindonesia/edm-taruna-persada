e6fc954bd4dae8f6d6787e48442ed55e1548d903
ardianita <ardianitaf@student.uns.ac.id>
2024-05-28 08:51:50 +0700

    make crud program regency

---
+++ app/Http/Controllers/Admin/ProgramRegencyController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Program;
+use App\Models\Regency;
+use Illuminate\Http\Request;
+
+class ProgramRegencyController extends Controller
+{
+    public function index()
+    {
+        $programs = Program::with('regencies')->get();
+
+        return view('programPerKota.index', [
+            'programs' => $programs,
+        ]);
+    }
+
+    public function create()
+    {
+        $regency = Regency::all();
+        $program = Program::all();
+
+        return view('programPerKota.create', [
+            'regencies' => $regency,
+            'programs' => $program,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'programId' => ['required', 'exists:programs,id'],
+            'regencyId' => ['required', 'exists:regencies,id'],
+        ], [
+            'programId.required' => 'Program harus diisi!',
+            'regencyId.required' => 'Kabupaten/Kota harus diisi!',
+        ]);
+
+        $kota = $request->regencyId;
+
+        foreach ($kota as $key => $value) {
+            $program = Program::find($request->programId);
+            $program->regencies()->attach($kota[$key]);
+        }
+
+        return redirect()->route('program-regency.index');
+    }
+}
