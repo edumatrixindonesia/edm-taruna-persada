796c358b5d2df7ca61f156387e24cb9548393662
ardianita <ardianitaf@student.uns.ac.id>
2024-05-28 15:44:35 +0700

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            MapelSeeder::class,
             ProgramSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
