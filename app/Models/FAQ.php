242cdea9e45992697703ba6dc4ebc837ad0763e9
ardianita <ardianitaf@student.uns.ac.id>
2024-06-06 15:31:58 +0700

    update sequence

--- app/Models/FAQ.php
+++ app/Models/FAQ.php
@@ -20,6 +20,15 @@ class FAQ extends Model
         parent::boot();
         static::creating(function ($model) {
             $model->id = Str::uuid();
+
+            if (static::count() < 1) {
+                $model->sequence = 1;
+                return;
+            }
+
+            $increment = static::latest()->first()->sequence;
+            $value = $increment + 1;
+            $model->sequence = $value;
         });
     }
 }
